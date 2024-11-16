75d4c45937248c6a72d7d34b7ddf9503068d7b8f
unknown <rafelbiondo@gmail.com>
2024-11-16 12:16:23 -0300

    começando  a trabalhar

---
+++ Exercicio_1/Exercicio_1.py
@@ -0,0 +1,9 @@
+# _______________________________________________________________________________________________________________________________________________________________________|
+# Desafio 1: Sistema de Locadora (Somente Python com POO)                                                                                                                |
+# Desenvolva um sistema simples de gerenciamento de filmes utilizando a orientação a objetos. Crie uma classe Filme com os atributos titulo, genero, ano e disponivel    |
+# (indica se o filme está disponível para locação). Implemente métodos para exibir as informações do filme e para marcar o filme como alugado ou disponível.             | 
+#                                                                                                                                                                        |
+# Requisitos:                                                                                                                                                            |
+# Crie a classe Filme com:Construtor para inicializar os atributos (titulo, genero, ano, disponível). Método exibir_informacoes que exibe os detalhes do filme.          |
+# Método alugar que marca o filme como indisponível. Método devolver que marca o filme como disponível.                                                                  |
+#________________________________________________________________________________________________________________________________________________________________________|
